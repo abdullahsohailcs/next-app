3a94747829d529dcebd9bbc4babbce0ad8294cc7
Mac <mac@174-16-0-48.hlrn.qwest.net>
2024-11-20 20:39:48 +0500

    new features

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
-
+import { deleteInvoice } from '@/app/lib/actions';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 export function Button({ children, className, ...rest }: ButtonProps) {
   return (
@@ -15,5 +16,18 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     >
       {children}
     </button>
+  );
+}
+
+export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+ 
+  return (
+    <form action={deleteInvoiceWithId}>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-4" />
+      </button>
+    </form>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: 'incremental', // Enable the partial prerendering feature
+  },
 };
 
 export default nextConfig;
